e72b8852ce3f704b2c32522b12a218c3fbc202ce
yujinkimmn <rqwuy8@naver.com>
2023-06-01 19:59:23 +0900

    dashboard.py 문구 수정

--- Prototype/📊Dashboard.py
+++ Prototype/📊Dashboard.py
@@ -405,11 +405,11 @@ with st.sidebar:
     # 오늘 날짜로 슬라이더 끝을 설정
     today = date.today()
     today_datetime = datetime.combine(today, datetime.min.time())
-    time = st.slider("날짜를 설정하시오",
+    time = st.slider("날짜를 설정하세요.",
                            value=(datetime(2021, 1, 1), today_datetime),
                            format="YYYY/MM/DD")
     
-    region_option = st.selectbox("지역을 선택하세요", 
+    region_option = st.selectbox("지역을 선택하세요.", 
                           ("서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시",
                            "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", 
                            "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도"))
